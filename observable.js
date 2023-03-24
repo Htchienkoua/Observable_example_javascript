13c71715576b48b6c3dc80d365b6b26ba76ab42e
Jurgis Bridžius <jurgis.bridzius@swedbank.lt>
2023-03-24 09:13:21 +0200

    observable examples

---
+++ observable.js
@@ -0,0 +1,22 @@
+import { Observable } from "rxjs";
+
+//Observable
+const obs = new Observable((subject) => {
+  //Push messages to the observer
+  subject.next(1);
+  subject.next(2);
+  subject.next("Jurgis");
+  subject.complete();
+});
+//Define an observer
+function observer(message) {
+  console.log(message);
+}
+
+//Attach the observer to the Observable
+obs.subscribe({
+  next: observer,
+  error: (err) => console.error(err),
+  complete: () => console.log("Observable completed"),
+});
+
